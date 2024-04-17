dd3fd29a04ab590dc3d05819f481e336c47d8ec7
Inam Ul Rehman <inam@dryermaster.com>
2024-04-17 08:52:24 -0400

    ReadMe added.

--- controllers/devices/disconnect/wsDisconnectionController.js
+++ controllers/devices/disconnect/wsDisconnectionController.js
@@ -7,16 +7,24 @@ const wsLogger = require("../../../lib/loggers/wsLogger");
 // Function to handle WebSocket disconnections
 const handleDisconnection = (ws) => {
     ws.on('close', async () => {
-        wsLogger.info(`Device ${ws.deviceId} disconnected with IP address ${ws.ipAddress}`);
+
         // Set connection flags
         ws.isAuthenticated = false;
         ws.isAlive = false;
+        ws.deviceId = null;
+        ws.deviceModel = null;
+        ws.ipAddress = null;
+        ws.locationLatitude = null;
+        ws.locationLongitude = null;
+        ws.firmwareVersion = null;
 
         try {
             // Update the device status to offline
             await Device.update({ isOnline: false }, { where: { deviceId: ws.deviceId } });
+            wsLogger.info(`Device ${ws.deviceId} is now offline.`);
         } catch (error) {
             console.error('Error handling disconnection:', error);
+            wsLogger.error(`Error handling disconnection: ${error.message}`);
         }
     });
 };
